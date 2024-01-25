2f7b8de8130fc1b5447f1fa5fe86174c3180106e
Mathieu <mathieu.thomassin@eleve.ensai.fr>
2024-01-25 16:51:41 +0100

    pause

--- TP1/src/classes/classes.py
+++ TP1/src/classes/classes.py
@@ -4,15 +4,6 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class Href(Base):
-    __tablename__ = 'hrefs'
-
-    id = Column(Integer, primary_key=True)
-    url = Column(String)
-    est_autorise = Column(Boolean)
-    
-    # Relationship with Page
-    pages = relationship("Page", secondary="page_href_association")
 
 class Page(Base):
     __tablename__ = 'pages'
@@ -26,21 +17,7 @@ class Page(Base):
     domaine_id = Column(Integer, ForeignKey('domaines.id'))
     domaine = relationship("Domaine", back_populates="pages")
 
-    # Relationship with Href
-    hrefs = relationship("Href", secondary="page_href_association")
-
-class PageHrefAssociation(Base):
-    __tablename__ = 'page_href_association'
-
-    page_id = Column(Integer, ForeignKey('pages.id'), primary_key=True)
-    href_id = Column(Integer, ForeignKey('hrefs.id'), primary_key=True)
-
-class Robot(Base):
-    __tablename__ = 'robots'
-
-    id = Column(Integer, primary_key=True)
-    autorise = Column(Boolean)
-
+ 
 class Domaine(Base):
     __tablename__ = 'domaines'
 
@@ -49,10 +26,6 @@ class Domaine(Base):
     
     # Relationship with Page
     pages = relationship("Page", back_populates="domaine")
-
-    # Relationship with Robot
-    robot_id = Column(Integer, ForeignKey('robots.id'))
-    robot = relationship("Robot")
 
 # Création de la base de données et de la structure
 engine = create_engine('sqlite:///example.db', echo=True)
