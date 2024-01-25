da3dfd249834956624ffe3d60bc0d8bfd7541526
unknown <colin.despouys@orange.fr>
2024-01-25 18:25:15 +0100

    test

--- TP1/src/get_urls_recursively.py
+++ TP1/src/get_urls_recursively.py
@@ -78,16 +78,16 @@ def get_urls_recursively(url):
 #         print(f"{url}, Last Modified: {lastmod}")
 #     print(type(all_urls_recursively))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     # Scraper récursivement toutes les URLs à partir d'un sitemap ou index de sitemap
     all_urls_recursively = get_urls_recursively("https://ensai.fr/sitemap_index.xml")
 
-    with open("urls.txt","w+") as f :
-        for url in all_urls_recursively :
-            f.write(url+"\n")
+    with open("urls.txt", "w+") as f:
+        for url, lastmod in all_urls_recursively:
+            f.write(f"{url}, Last Modified: {lastmod}\n")
             
-    for url in all_urls_recursively : 
-        print(url)
+    for url, lastmod in all_urls_recursively:
+        print(f"{url}, Last Modified: {lastmod}")
     print(type(all_urls_recursively))
     
     
